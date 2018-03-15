6320449c5a7c3ceb3d0cdb678bb33a3b9ca6cda5
ginnyli <meiqing.virginia.li@gmail.com>
2018-03-15 02:06:41 -0400

    functions

--- assignment/midtermFunctions.js
+++ assignment/midtermFunctions.js
@@ -1,3 +1,12 @@
 /* ================================
 Week 6 Assignment: Midterm Functions + Signatures
 ================================ */
+
+//the following functions can be used
+L.geoJSON(geojsonFeature).addTo(map);
+
+var freeway = L.geoJson(geojsonFeature, {filter: freewayFilter}).addTo(map);
+function freewayFilter(feature) {
+  if (feature.properties.Freeway === "Yes") return true;
+}
+//similary functions apply to other road classifications
